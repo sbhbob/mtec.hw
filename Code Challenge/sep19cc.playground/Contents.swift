651aaa2a0bd429633b3d0cd553f2b4398cda0c26
Skyler Hope <skylerhope@Skylers-MacBook-Air.local>
2022-09-19 07:50:25 -0600

    sep19cc

---
+++ Code Challenge/sep19cc.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+func whichCentury(is year: Int) -> String {
+    if year 1...100 {
+        return("\(year) is in the first century")
+    }
+    else if year 101...200 {
+        return("\(year) is in the second century")
+    }
+    else if year 201...300 {
+        return("\(year) is in the third century")
+    }
+    else if year 301...400 {
+        return("\(year) is in the fourth century")
+    }
+    else if year 401...500 {
+        return("\(year) is in the fifth century")
+    }
+    else if year 501...600 {
+        return("\(year) is in the sixth century")
+    }
+    else if year 601...700 {
+        return("\(year) is in the seventh century")
+    }
+    else if year 701...800 {
+        return("\(year) is in the eighth century")
+    }
+    else if year 801...900 {
+        return("\(year) is in the ninth century")
+    }
+    else if year 901...1000 {
+        return("\(year) is in the tenth century")
+    }
+    else if year 1001...1100 {
+        return("\(year) is in the eleventh century")
+    }
+    else if year 1101...1200 {
+        return("\(year) is in the twelfth century")
+    }
+    else if year 1201...1300 {
+        return("\(year) is in the thirteenth century")
+    }
+    else if year 1301...1400 {
+        return("\(year) is in the fourteenth century")
+    }
+    else if year 1401...1500 {
+        return("\(year) is in the fifteenth century")
+    }
+    else if year 1501...1600 {
+        return("\(year) is in the sixteenth century")
+    }
+    else if year 1601...1700 {
+        return("\(year) is in the seventeenth century")
+    }
+    else if year 1701...1800 {
+        return("\(year) is in the eighteenth century")
+    }
+    else if year 1801...1900 {
+        return("\(year) is in the nineteenth century")
+    }
+    else if year 1901...2000 {
+        return("\(year) is in the twentieth century")
+    }
+    else if year 2001...2100 {
+        return("\(year) is in the twenty-first century")
+    }
+}
